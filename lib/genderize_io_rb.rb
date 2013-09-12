02e9cc89891cb087739d433c91993eb1d2bfae55
Kasper Johansen <kaspernj@gmail.com>
2013-09-12 12:17:13 +0200

    Added support for ActiveSupport caches.

--- lib/genderize_io_rb.rb
+++ lib/genderize_io_rb.rb
@@ -12,7 +12,12 @@ class GenderizeIoRb
     return ::GenderizeIoRb.const_get(name)
   end
   
+  INITIALIZE_VALID_ARGS = [:cache_as, :cache_db]
   def initialize(args = {})
+    args.each do |key, val|
+      raise "Invalid key: '#{key}'." unless INITIALIZE_VALID_ARGS.include?(key)
+    end
+    
     @args = args
     @http = Http2.new(:host => "api.genderize.io")
     
@@ -21,6 +26,8 @@ class GenderizeIoRb
     if @cache_db
       Baza::Revision.new.init_db(:db => @cache_db, :schema => GenderizeIoRb::DatabaseSchema::SCHEMA)
     end
+    
+    @cache_as = args[:cache_as]
   end
   
   def info_for_name(name)
@@ -29,35 +36,54 @@ class GenderizeIoRb
     
     # If a database-cache is enabled, try to look result up there first.
     if @cache_db
-      res = @cache_db.single(:genderize_io_rb_cache, {:name => name_lc})
-      if res
-        return ::GenderizeIoRb::Result.new(
-          :data => JSON.parse(res[:result]),
+      cache_db_res = @cache_db.single(:genderize_io_rb_cache, {:name => name_lc})
+      if cache_db_res
+        res = ::GenderizeIoRb::Result.new(
+          :data => JSON.parse(cache_db_res[:result]),
           :genderize_io_rb => self
         )
       end
     end
     
-    http_res = @http.get("?name=#{CGI.escape(name)}")
-    json_res = JSON.parse(http_res.body)
+    cache_as_key = "genderize_io_rb_#{name_lc}"
+    if @cache_as
+      cache_as_res = @cache_as.read(cache_as_key)
+      puts "CacheAsRes: #{cache_as_res}"
+      
+      if cache_as_res
+        res = ::GenderizeIoRb::Result.new(
+          :data => JSON.parse(cache_as_res),
+          :genderize_io_rb => self
+        )
+      end
+    end
     
-    raise GenderizeIoRb::Errors::NameNotFound, "Name was not found on Genderize.io: '#{name}'." unless json_res["gender"]
-    
-    res = ::GenderizeIoRb::Result.new(
-      :data => json_res,
-      :genderize_io_rb => self
-    )
+    unless res
+      http_res = @http.get("?name=#{CGI.escape(name)}")
+      json_res = JSON.parse(http_res.body)
+      
+      raise GenderizeIoRb::Errors::NameNotFound, "Name was not found on Genderize.io: '#{name}'." unless json_res["gender"]
+      
+      res = ::GenderizeIoRb::Result.new(
+        :data => json_res,
+        :genderize_io_rb => self
+      )
+    end
     
     # Save result to the database cache.
-    if @cache_db
+    if @cache_db && !cache_db_res
       @cache_db.insert(:genderize_io_rb_cache, {
         :name => name_lc,
         :result => http_res.body,
         :created_at => Time.now
-      }) unless @cache_db.single(:genderize_io_rb_cache, {:name => name_lc})
+      })
     end
     
-    return res
+    if @cache_as && !cache_as_res
+      @cache_as.write(cache_as_key, http_res.body)
+    end
+    
+    return {:result => res, :cache_db => @cache_db_res, :cache_as => cache_as_res}
   end
   
   def destroy
